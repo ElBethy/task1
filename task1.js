dd95689a732fab59ff2d7e057586766cc9e723af
Olalere Kareem <olalereolatunbosun@gmail.com>
2022-08-24 14:57:35 -0700

    updated commit

--- task1.js
+++ task1.js
@@ -1,4 +1,6 @@
+//WRITE A FOR LOOP
 for(i=0;i<+0;i++){
+//USE AN IF CONDITIONAL STATEMENT TO RETURN EVEN AND ODD NUMBER
     if(1%2 == 0){
         const newH1 = document.createElement("h1");
         newH1.innerHTML = `${i} is even`;
--- task2/task2.js
+++ task2/task2.js
@@ -1,7 +1,10 @@
+//WRITE AN ARRAY OF NUMBERS
 const array = [2, 45, 6, 32, 45, 6, 43, 9];
 
+//DECLARE A VARIABLE TO CHECK FOR THE LARGEST NUMBER
 let largNum = array[0]
 for(i=0;i<array.length;i++){
     largNum = largNum > array[i] ? largNum : array[i] 
 }
-alert(largNum);
+
+alert(`output: ${largNum}`);
