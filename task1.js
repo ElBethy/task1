e6bce1c3d7e180eb0bfe7d6b0051c65350397d39
Elbeth <tonyfav24@gmail.com>
2022-08-24 16:02:29 +0100

    made functional

--- task1.js
+++ task1.js
@@ -1,10 +1,12 @@
-for(i=0;i<+0;i++){
-    if(1%2 == 0){
+for(i=0; i<=15; i++){
+    // this sction checks for even number
+    if(i%2 == 0){
         const newH1 = document.createElement("h1");
         newH1.innerHTML = `${i} is even`;
         document.body.appendChild(newH1)
     }
-    if(1%2 != 0){
+    // this sction checks for odd number
+    if(i%2 != 0){
         const newH1 = document.createElement("h1");
         newH1.innerHTML = `${i} is odd`;
         document.body.appendChild(newH1)
